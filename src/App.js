929b97bd34f5fdbe28af15fbc151071281b70b7f
Wenkui Fu <fuwenkui@gmail.com>
2019-07-06 15:37:42 +1000

    get rid of animation hinge

--- src/App.js
+++ src/App.js
@@ -57,16 +57,11 @@ class App extends Component {
   handleConfirmDone() {
 
     this.setState({
-      visible: !this.state.visible
-    })
-    setTimeout(() =>  this.setState({
       targetWeight: 0,
       nowWeight: -1,
       data: [],
-      visible: !this.state.visible
-    }), 2000)
-   ;
-    
+    });
+   
   }
 
   render() {
@@ -92,7 +87,7 @@ class App extends Component {
       )
     } else if (this.state.nowWeight < 1) {
         return (
-          <Animated animationIn="zoomIn" animationOut="hinge" animationInDuration={2000} animationOutDuration={2000} isVisible={this.state.visible}>
+          <Animated animationIn="zoomIn"  animationInDuration={2000} isVisible={this.state.visible}>
             <div className="App">
               <div className="done">
                 <h3>Mission Accomplished!</h3>
